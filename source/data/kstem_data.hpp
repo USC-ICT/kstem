bc9d2713e68d07f8be1c288f9a516bfdef12bdbd
leuski-ict <leuski@ict.usc.edu>
2016-04-07 08:23:02 -0700

    tweaking the names.

--- source/data/kstem_data.hpp
+++ source/data/kstem_data.hpp
@@ -5,8 +5,32 @@
 //  Copyright (c) 2015 Anton Leuski & ICT/USC. All rights reserved.
 //
 
-#ifndef alib_kstem_data_hpp
-#define alib_kstem_data_hpp
+/*
+ This data is taken from kstem 0.8 distribution.
+ 
+ Copyright 1990,1991 by the
+ Information Retrieval Laboratory,
+ University of Massachusetts,
+ Amherst MA. 01003
+ All Rights Reserved.
+ 
+ Kstem is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Library General Public License as published by the
+ Free Software Foundation; either version 2 of the  License, or (at your
+ option) any later version.
+ 
+ Kstem is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License for
+ more details.
+ 
+ The GNU Library General Public License is available at part of the Linux
+ system, or by writing to the Free Software Foundation, Inc., 59 Temple Place,
+ Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __kstem_kstem_data_hpp__
+#define __kstem_kstem_data_hpp__
 
 namespace kstem {
 
@@ -30,4 +54,4 @@ extern const unsigned proper_nouns_size;
 
 }
 
-#endif
+#endif // __kstem_kstem_data_hpp__
